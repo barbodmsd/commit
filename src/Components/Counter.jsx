c218cab90a11227886a794efb2dc1026ff31e942
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-08 16:34:32 +0330

    button

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -7,13 +7,18 @@ export default class Counter extends Component {
             count:0
         }
     }
-    handlePlus
+    handlePlus(){
+        this.setState({count:this.state.count+1})
+    }
+    handleMines(){
+        this.setState({count:this.state.count-1})
+    }
   render() {
     return (
       <div>
         <h1>count:{this.state.count}</h1>
-        <button className='btn btn-danger '>-</button>
-        <button className='btn btn-primary '>+</button>
+        <button className='btn btn-danger ' disabled={!this.state.count} onClick={this.handleMines.bind(this)}>-</button>
+        <button className='btn btn-primary 'onClick={this.handlePlus.bind(this)}>+</button>
       </div>
     )
   }
