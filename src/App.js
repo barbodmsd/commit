530d80d09f8cee56a042f2dfc6b4ec837135a423
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-09 23:00:59 +0330

    start chat app

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Condition from './Components/Condition'
-
-export default function App() {
-  return (
-    <div><Condition/></div>
-  )
-}
---
+++ src/Components/CleanChat.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CleanChat extends Component {
+  render() {
+    return (
+      <div>CleanChat</div>
+    )
+  }
+}
