c803189078cddec899d2b871c278d88c5df8edbb
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-09 23:22:51 +0330

    update

--- src/Components/CleanChat.jsx
+++ src/Components/CleanChat.jsx
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Main from './Main'
+import Footer from './Footer'
 
 export default class CleanChat extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            barbodTitle:"Saleh",
+            barbodMassageList:[
+                {
+                    type:'sent',
+                    massage:'Good morning, sir. What can I do for you?',
+                    time:'11:37:08 AM'
+                },
+                {
+                    type:'receive',
+                    massage:'Well, I AM just looking around.',
+                    time:'11:39:08 AM'
+                },
+                {
+                    type:'sent',
+                    massage:'If necessary, please ask me. Barbod',
+                    time:'11:40:08 AM'
+                },
+            ],
+            barbodAvatar:{
+                Saleh:'https://bootdey.com/img/Content/avatar/avatar1.png',
+                barbod:'https://bootdey.com/img/Content/avatar/avatar2.png',
+            }
+        }
+    }
   render() {
     return (
         <div className="container bootstrap snippets">
