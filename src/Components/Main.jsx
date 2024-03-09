f8fa9b771d69ca4349a339bb60953199cd6ddce3
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-09 23:49:19 +0330

    start footer

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Chat from './Chat'
+import React from "react";
+import Chat from "./Chat";
 
-export default function Main({massageList,avatar}) {
-  const massageItems=massageList?.map((e,index)=>{
-    const isLeft=e.type==='receive'
-    return <Chat key={index} massage={e.massage} isLeft={isLeft} time={e.time} img={isLeft?avatar.saleh:avatar.barbod}/>
-  })
+export default function Main({ massageList, avatar }) {
+  const massageItems = massageList?.map((e, index) => {
+    const isLeft = e.type === "receive";
+    return (
+      <Chat
+        key={index}
+        massage={e.massage}
+        isLeft={isLeft}
+        time={e.time}
+        img={isLeft ? avatar.saleh : avatar.barbod}
+      />
+    );
+  });
   return (
     <div className="panel-body">
-    <div className="chats">
-      {massageItems}
+      <div className="chats">{massageItems}</div>
     </div>
-  </div>
-  )
+  );
 }
