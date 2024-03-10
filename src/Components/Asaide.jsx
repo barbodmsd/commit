d08b63d806a2c1b562f4c24e4ba75ca88dc4b50a
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-10 12:08:19 +0330

    end sidebar

--- src/Components/Asaide.jsx
+++ src/Components/Asaide.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-export default function Asaide() {
+export default function Asaide({posts,handleTitle}) {
+    const listTitle=posts?.map((e,index)=><li key={index} onClick={()=>handleTitle(index)}>{e.title}</li>)
   return (
     <div>
 
 <aside>
             <ul>
-                <li>Post 1</li>
-                <li>Post 2</li>
-                <li>Post 3</li>
+                {listTitle}
             </ul>
 
         </aside>
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 export default class Main extends Component {
+    constructor(props){
+        super(props)
+        this.state.
+    }
   render() {
     return (
         <div class="container">
