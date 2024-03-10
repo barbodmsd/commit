9cd7f6eb8c86dac76eda3ba438c55d56ac1fbac7
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-10 11:51:43 +0330

    blog cmp

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -3,6 +3,13 @@ import Asaide from './Asaide'
 import Main from './Main'
 
 export default class Blog extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            posts:null,
+            currentIndexPost:null
+        }
+    }
   render() {
     return (
       <>
