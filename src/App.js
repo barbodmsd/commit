02022a7082e8cc00b09c4d27e8d38088d65cd8c1
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-10 13:22:22 +0330

    start posts project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import Blog from './Components/Blog'
-import './App.css'
+
 export default function App() {
   return (
-    <div><Blog/></div>
+    <div>App</div>
   )
 }
--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,41 +1,9 @@
-import React, { Component } from "react";
-import Asaide from "./Asaide";
-import Main from "./Main";
+import React, { Component } from 'react'
 
 export default class Blog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: null,
-      currentIndexPost: null,
-    };
-  }
-  componentDidMount() {
-    (async () => {
-      try {
-        const res = await fetch("http://localhost:3000/posts");
-        const data = await res.json();
-        this.setState({ posts: data, currentIndexPost: 0 });
-      } catch (err) {
-        alert(err);
-      }
-    })();
-  }
-  handleTitle(index){
-    this.setState({currentIndexPost:index})
-  }
   render() {
     return (
-      <>
-        {this.state.posts ? (
-          <>
-            <Asaide posts={this.state.posts} handleTitle={this.handleTitle.bind(this)}/>
-            <Main post={this.state.posts[this.state.currentIndexPost]} />
-          </>
-        ) : (
-          <h1>Loading...</h1>
-        )}
-      </>
-    );
+      <div>Blog</div>
+    )
   }
 }
