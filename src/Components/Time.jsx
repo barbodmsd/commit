21b247bd2177de4019a03351780b683fd72824f3
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-11 14:08:01 +0330

    start time cmp

--- src/Components/Time.jsx
+++ src/Components/Time.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Time extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            time:new Date().toLocaleTimeString()
+        }
+    }
+    co
   render() {
     return (
       <div>Time</div>
