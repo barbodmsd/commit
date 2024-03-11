db8492fa2984ace3ff0cd29b89815c557bb690ff
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-11 14:32:21 +0330

    end counter cmp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Counter from './Components/Counter'
 
 export default function App() {
   return (
-    <div>App</div>
+    <Counter/>
   )
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
   return (
-    <div>Counter</div>
-  )
+    <div>
+      <h2>count:{count}</h2>
+      <button
+        className="btn btn-danger"
+        disabled={!count}
+        onClick={() => setCount(count - 1)}
+      >
+        -
+      </button>
+      <button className="btn btn-success" onClick={increase}>
+        +
+      </button>
+    </div>
+  );
 }
