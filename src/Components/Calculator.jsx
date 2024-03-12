0bb61db1508f47987e28116c55307a2f3c82aed0
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-12 18:04:30 +0330

    update calculator

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function Calculator() {
+  const [inp1, setInp1] = useState("");
+  const [inp2, setInp2] = useState("");
+  const [operator, setOperator] = useState("");
+
   return (
-    <div>Calculator</div>
-  )
+    <div>
+      <form>
+        <input
+          type="text"
+          value={inp1}
+          onChange={(e) => setInp1(e.target.value)}
+        />
+        <input
+          type="text"
+          value={inp2}
+          onChange={(e) => setInp2(e.target.value)}
+        />
+        <select value={operator} onChange={(e) => setOperator(e.target.value)}>
+          <option>choose Operator </option>
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="/">/</option>
+          <option value="*">*</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,24 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function Result() {
-    const [inp1,setInp1]=useState('')
-    const [inp2,setInp2]=useState('')
-    const [operator,setOperator]=useState('')
-
   return (
-    <div>
-        <form >
-            <input type="text" value={inp1} onChange={(e)=>setInp1(e.target.value)} />
-            <input type="text" value={inp2} onChange={(e)=>setInp2(e.target.value)} />
-            <select onChange={(e)=>setOperator(e.target.value)}>
-                <option >choose Operator </option>
-                <option value="+">+</option>
-                <option value="-">-</option>
-                <option value="/">/</option>
-                <option value="*">*</option>
-            </select>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
+    <div>Result</div>
   )
 }
