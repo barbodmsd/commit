e9ac5d33b0713d820b1256797e8fbb0717135007
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-12 18:18:58 +0330

    end styling

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Calculator() {
-  const [inp1, setInp1] = useState("");
-  const [inp2, setInp2] = useState("");
+  const [inp1, setInp1] = useState('');
+  const [inp2, setInp2] = useState('');
   const [operator, setOperator] = useState("");
 
   return (
-    <div>
-      <form>
+    <div >
+      <form >
         <input
           type="text"
           value={inp1}
+          placeholder="Number:"
           onChange={(e) => setInp1(e.target.value)}
         />
         <input
           type="text"
           value={inp2}
+          placeholder="Number:"
+
           onChange={(e) => setInp2(e.target.value)}
         />
         <select value={operator} onChange={(e) => setOperator(e.target.value)}>
