27be4ca97b059708b33d3702821a5fe86fc7a24e
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-12 12:43:03 +0330

    clock cmp end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Calculator from './Components/Calculator'
+import Clock from './Components/Clock'
 
 export default function App() {
   return (
-    <div><Calculator/></div>
+    <div>
+      <Clock/>
+    </div>
   )
 }
-
--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import Time from "./Time";
 
 export default function Clock() {
+  const [showTime, setShowTime] = useState(true);
   return (
-    <div>Clock</div>
-  )
+    <div>
+      {showTime && <Time />}
+      <button
+        className={`btn ${showTime ? "btn-danger " : "btn-success"}`}
+        onClick={!showTime}
+      >
+        ${showTime ? "Hide time" : "Show time"}
+      </button>
+    </div>
+  );
 }
