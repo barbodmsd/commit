ab0c96af1dbdd9c3cf9d2c04775863b0e55c3fcf
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-12 18:00:48 +0330

    import calculator in app cmp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Calculator from './Components/Calculator'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div><Calculator/></div>
   )
 }
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Result() {
+    const [inp1,setInp1]=useState('')
+    const [inp2,setInp2]=useState('')
+    const [operator,setOperator]=useState('')
+
   return (
-    <div>Result</div>
+    <div>
+        <form >
+            <input type="text" value={inp1} onChange={(e)=>setInp1(e.target.value)} />
+            <input type="text" value={inp2} onChange={(e)=>setInp2(e.target.value)} />
+            <select onChange={(e)=>setOperator(e.target.value)}>
+                <option >choose Operator </option>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="/">/</option>
+                <option value="*">*</option>
+            </select>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
   )
 }
