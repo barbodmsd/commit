bfde661882a74ddec865baee54af21f6dc43ed24
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-12 13:52:10 +0330

    enf calculator cmp

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import Result from "./Result";
 
 export default function Calculator() {
+  const [firstInp, setFirstInp] = useState();
+  const [secondInp, setSecondInp] = useState();
+  const [select, setSelect] = useState();
+  const [click, setClick] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setClick(!click);
+  };
   return (
     <div>
-        <form >
-            <input type="text" />
-            <input type="text" />
-            <select>
-                <option value="+">+</option>
-                <option value="-">-</option>
-                <option value="/">/</option>
-                <option value="*">*</option>
-            </select>
-            <button type="submit"></button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setFirstInp(e.target.value)} />
+        <input type="text" onChange={(e) => setSecondInp(e.target.value)} />
+        <select onChange={(e) => setSelect(e.target.value)}>
+          <option value="">Choose Operator</option>
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="/">/</option>
+          <option value="*">*</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+      {click && <Result a={firstInp} b={secondInp} operator={select} />}
     </div>
-  )
+  );
 }
---
+++ src/Components/Result.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Result({a,b,operator}) {
+    const calculator={
+        switch(operator){
+            
+        
+        }
+    }
+  return (
+    <div>
+        {alert(a, b)}
+    </div>
+  )
+}
