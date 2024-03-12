619677e19419e5e1f25654683838a9d8f95458ea
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-12 20:21:14 +0330

    update

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 
 export default function Result({ a, b, operator }) {
-  const [answer,setAnswer]=useState()
-    const result = () => {
-      switch (operator) {
-        case "+":
-          return a + b;
-          break;
-        case "-":
-          return a - b;
-          break;
-        case "/":
-          return a / b;
-          break;
-        case "*":
-          return a * b;
-          break;
-      }
-    };
-  return (
-      <h2>Answer:{result}</h2>
-    
-  );
+  const [answer, setAnswer] = useState();
+  const result = () => {
+    switch (operator) {
+      case "+":
+        return +a + +b;
+        break;
+      case "-":
+        return a - b;
+        break;
+      case "/":
+        return a / b;
+        break;
+      case "*":
+        return a * b;
+        break;
+      default:
+        return "error";
+    }
+  };
+  return <h2>result:{result}</h2>;
 }
