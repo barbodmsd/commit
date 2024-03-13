d25390105b6845a9d5b8b7f3a2f50699e76be5eb
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-13 16:41:10 +0330

    end arrow cmp

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,21 +4,21 @@ export default function Main({hero}) {
     const {name,img,level,description,units}=hero
   return (
     <div className={`clash-card ${name}`}>
-          <div className={`clash-card__image clash-card__image--barbarian`}>
+          <div className={`clash-card__image clash-card__image--${name}`}>
             <img
               src={img}
-              alt={`barbarian`}
+              alt={`${name}`}
             />
           </div>
-          <div className={`clash-card__level clash-card__level--barbarian`}>
+          <div className={`clash-card__level clash-card__level--${name}`}>
             {level}
           </div>
-          <div className={`clash-card__unit-name`}>The Barbarian</div>
+          <div className={`clash-card__unit-name`}>The ${name}</div>
           <div className={`clash-card__unit-description`}>
             {description}
           </div>
 
-          <div className={`clash-card__unit-stats clash-card__unit-stats--barbarian clearfix`}>
+          <div className={`clash-card__unit-stats clash-card__unit-stats--${name} clearfix`}>
             <div className={`one-third`}>
               <div className={`stat`}>
                 20<sup>S</sup>
