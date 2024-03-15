c2891cd63631b86d02f508135e9111fdf264b510
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-15 20:16:37 +0330

    before units

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,39 +3,37 @@ import React from 'react'
 export default function Main({hero}) {
     const {name,img,description,level,units}=hero
   return (
-    <div class="clash-card barbarian">
-    <div class="clash-card__image clash-card__image--barbarian">
+    <div className={`clash-card ${name}`}>
+    <div className={`clash-card__image clash-card__image--${name}`}>
       <img
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/barbarian.png"
-        alt="barbarian"
+        src={img}
+        alt={`${name}`}
       />
     </div>
-    <div class="clash-card__level clash-card__level--barbarian">
-      Level 4
+    <div className={`clash-card__level clash-card__level--${name}`}>
+     {level}
     </div>
-    <div class="clash-card__unit-name">The Barbarian</div>
-    <div class="clash-card__unit-description">
-      The Barbarian is a kilt-clad Scottish warrior with an angry,
-      battle-ready expression, hungry for destruction. He has Killer
-      yellow horseshoe mustache.
+    <div className={`clash-card__unit-name`}>The ${name}</div>
+    <div className={`clash-card__unit-description`}>
+     {description}
     </div>
 
-    <div class="clash-card__unit-stats clash-card__unit-stats--barbarian clearfix">
-      <div class="one-third">
-        <div class="stat">
+    <div className={`clash-card__unit-stats clash-card__unit-stats--${name} clearfix`}>
+      <div className={`one-third`}>
+        <div className={`stat`}>
           20<sup>S</sup>
         </div>
-        <div class="stat-value">Training</div>
+        <div className={`stat-value`}>Training</div>
       </div>
 
-      <div class="one-third">
-        <div class="stat">16</div>
-        <div class="stat-value">Speed</div>
+      <div className={`one-third`}>
+        <div className={`stat`}>16</div>
+        <div className={`stat-value`}>Speed</div>
       </div>
 
-      <div class="one-third no-border">
-        <div class="stat">150</div>
-        <div class="stat-value">Cost</div>
+      <div className={`one-third no-border`}>
+        <div className={`stat`}>150</div>
+        <div className={`stat-value`}>Cost</div>
       </div>
     </div>
   </div>
