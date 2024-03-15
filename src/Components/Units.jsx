7d7a32241002cd9129b3baba3e25c8312691a789
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-15 20:18:20 +0330

    strat unit cmp

---
+++ src/Components/Units.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Units() {
+  return (
+    <div className={`one-third`}>
+    <div className={`stat`}>16</div>
+    <div className={`stat-value`}>Speed</div>
+  </div>
+  )
+}
