c6247196071f3019a3efcd6211ca5c528d2d8e60
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-15 20:02:16 +0330

    get heroes and props to main

--- src/Components/Arrow.jsx
+++ src/Components/Arrow.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Arrow() {
+export default function Arrow({}) {
   return (
     <div class="arrow-wrapper">
     <div class="round">
--- src/Components/Sliders.jsx
+++ src/Components/Sliders.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Arrow from "./Arrow";
 import Main from "./Main";
+import { DNA } from "react-loader-spinner";
 
 export default function Sliders() {
-    
+  const [heroes, setHeroes] = useState();
+  const [heroIndex, setHeroIndex] = useState(0);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch("http://localhost:3000/heroes");
+        const data = await res.json();
+        setHeroes(data);
+      } catch (error) {
+        alert(err);
+      }
+    })();
+  }, []);
   return (
     <div class="slide-container">
       <div class="wrapper">
-        <Arrow />
-        <Main />
-        <Arrow />
+        {heroes ? (
+          <>
+            <Arrow />
+            <Main heroes={heroes[heroIndex]} />
+            <Arrow />
+          </>
+        ) : (
+          <div
+            className="d-flex position-fixed justify-content-center align-items-center "
+            style={{ inset: 0 }}
+          >
+            <DNA width={"100px"} height={"100px"} />
+          </div>
+        )}
       </div>
     </div>
   );
