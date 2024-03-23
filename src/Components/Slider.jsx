8ff645e3781e0f96b67261bfb4a17d4fedfb4774
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-23 16:29:09 +0430

    create card cmp

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,48 +1,14 @@
 import React from "react";
 import Arrow from "./Arrow";
+import Card from "./Card";
 
 export default function Slider() {
   return (
     <div class="slide-container">
       <div class="wrapper">
-    <Arrow/>
-        <div class="clash-card barbarian">
-          <div class="clash-card__image clash-card__image--barbarian">
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/barbarian.png"
-              alt="barbarian"
-            />
-          </div>
-          <div class="clash-card__level clash-card__level--barbarian">
-            Level 4
-          </div>
-          <div class="clash-card__unit-name">The Barbarian</div>
-          <div class="clash-card__unit-description">
-            The Barbarian is a kilt-clad Scottish warrior with an angry,
-            battle-ready expression, hungry for destruction. He has Killer
-            yellow horseshoe mustache.
-          </div>
-
-          <div class="clash-card__unit-stats clash-card__unit-stats--barbarian clearfix">
-            <div class="one-third">
-              <div class="stat">
-                20<sup>S</sup>
-              </div>
-              <div class="stat-value">Training</div>
-            </div>
-
-            <div class="one-third">
-              <div class="stat">16</div>
-              <div class="stat-value">Speed</div>
-            </div>
-
-            <div class="one-third no-border">
-              <div class="stat">150</div>
-              <div class="stat-value">Cost</div>
-            </div>
-          </div>
-        </div>
-       <Arrow/>
+        <Arrow />
+        <Card />
+        <Arrow />
       </div>
     </div>
   );
