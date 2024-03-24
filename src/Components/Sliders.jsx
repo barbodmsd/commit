b372da268fbabecfc01d79a8cc8fb02493091b6b
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 15:02:57 +0430

    fetch

--- src/Components/Sliders.jsx
+++ src/Components/Sliders.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Arrow from "./Arrow";
 import Main from "./Main";
 
 export default function Sliders() {
+    const [heroes,setHeroes]=useState(null)
+    const [index,setIndex]=useState(null)
+    
   return (
     <div class="slide-container">
       <div class="wrapper">
