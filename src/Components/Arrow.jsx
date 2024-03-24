15abe089754c52ade5bcb4a0a2c5b2e5062b3c97
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 15:00:03 +0430

    end arrow cmp

--- src/Components/Arrow.jsx
+++ src/Components/Arrow.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function Arrow() {
+export default function Arrow({direction,handleClick}) {
   return (
-    <div>Arrow</div>
+    <div className="arrow-wrapper" onClick={handleClick}>
+    <div className="round">
+      <div id="cta">
+        <span className={`arrow ${direction}`}></span>
+      </div>
+    </div>
+  </div>
   )
 }
