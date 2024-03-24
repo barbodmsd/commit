42a08a2be1effc9a70acb9764a1bf191797d47ff
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 17:14:01 +0430

    update post cmp

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { DNA } from "react-loader-spinner";
 
 export default function Post() {
-    const [postId,setPostId]=useState(1)
-    const [loading,setLoading]=useState(true)
-    const [title,setTitle]=useState("")
-    const [toast,setToast]=useState({type:'info',message:''})
+  const [postId, setPostId] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState("");
+  const [toast, setToast] = useState({ type: "info", message: "" });
   return (
-    <div>Post</div>
-  )
+    <div>
+      <input type="number" placeholder="Enter number:" />
+      {loading ? (
+        <div
+          className="d-flex justify-content-center  align-items-center position-fixed "
+          style={{ inset: 0 }}
+        >
+          <DNA />
+        </div>
+      ) : (
+        <div
+          className="d-flex justify-content-center  align-items-center position-fixed "
+          style={{ inset: 0 }}
+        >
+            <h2>{title}</h2>
+        </div>
+      )}
+    </div>
+  );
 }
