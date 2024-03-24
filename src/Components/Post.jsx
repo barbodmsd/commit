69e3c1028836bd6515d9af98a3f80dd4c40640ac
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 19:22:07 +0430

    fetch

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DNA } from "react-loader-spinner";
 
 export default function Post() {
@@ -6,13 +6,38 @@ export default function Post() {
   const [loading, setLoading] = useState();
   const [title, setTitle] = useState();
   const [toast, setToast] = useState({ type: "info", message: "" });
-  const handleChange=(e)=>{
-    setPostId(e.target.value)
-    setLoading(true)
-    setTitle('')
-  }
-  return <div>
-    <input type="number"  />
-    {loading?<div><DNA/></div>:<h5>{title}</h5>}
-  </div>;
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${postId}`
+        );
+        const data = await res.json();
+        if (data.title) {
+          setLoading(false);
+          setTitle(data.title);
+          setToast({ type: "success", message: `Data Loaded width post id ${postId}` });
+        }else{
+            throw new Error(`data failed with post id ${postId}`)
+        }
+      } catch (error) {}
+    })();
+  }, [postId]);
+  const handleChange = (e) => {
+    setPostId(e.target.value);
+    setLoading(true);
+    setTitle("");
+  };
+  return (
+    <div>
+      <input type="number" />
+      {loading ? (
+        <div>
+          <DNA />
+        </div>
+      ) : (
+        <h5>{title}</h5>
+      )}
+    </div>
+  );
 }
