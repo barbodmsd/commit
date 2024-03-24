89bbfde8aa15d9858f11579c1f28d662247f0af4
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 15:08:32 +0430

    handle click func

--- src/Components/Sliders.jsx
+++ src/Components/Sliders.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Arrow from "./Arrow";
 import Main from "./Main";
 
 export default function Sliders() {
     const [heroes,setHeroes]=useState(null)
     const [index,setIndex]=useState(null)
+    useEffect(async()=>{
+        try {
+            const res=await fetch('http://localhost:3000/heroes')
+            const data=await res.json()
+            setHeroes(data)
+            setIndex(0)
+        } catch (error) {
+            alert(error)
+        }
+    },[])
     
   return (
     <div class="slide-container">
       <div class="wrapper">
-       <Arrow/>
+       <Arrow direction={'prev'} handleClick={handleClick}/>
         <Main/>
-        <Arrow/>
+        <Arrow direction={'next'} handleClick={handleClick}/>
       </div>
     </div>
   );
