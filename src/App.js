a78c013603bb2abc7b54e7a933746d68ac4ccd73
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 17:05:26 +0430

    start post project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Sliders from './Components/Sliders'
+import Ref from './Components/Ref'
 
 export default function App() {
   return (
-    <Sliders/>
+   <Post
   )
 }
--- src/Components/Ref.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Ref() {
-  return (
-    <div>Ref</div>
-  )
-}
