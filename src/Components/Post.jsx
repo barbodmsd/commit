696a3845bf9fe4cb9d8308d2e99031e2a7f7bd19
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-24 19:17:42 +0430

    write the states

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DNA } from "react-loader-spinner";
 
 export default function Post() {
-  return (
-    <div>Post</div>
-  )
+  const [postId, setPostId] = useState();
+  const [loading, setLoading] = useState();
+  const [title, setTitle] = useState();
+  const [toast, setToast] = useState({ type: "info", message: "" });
+  const handleChange=(e)=>{
+    setPostId(e.target.value)
+    se
+  }
+  return <div>
+    <input type="number"  />
+    {loading?<div><DNA/></div>:<h5>{title}</h5>}
+  </div>;
 }
