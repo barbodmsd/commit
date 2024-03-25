3e95064812de3bdde6b7eb86e8258b1a34764210
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-25 19:00:06 +0430

    write all states

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Post() {
+    const [postId,setPostID]=useState(1)
+    const [loading,setLoading]=useState()
+    const [title,setTitle]=useState('')
+    const [toast,setToast]=useState({type:'info',massage:''})
   return (
     <div>Post</div>
   )
