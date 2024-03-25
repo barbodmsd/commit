93cf619e7441c4bd9ca893c71b3e1c24b6bc9b43
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-25 20:42:23 +0430

    htmls

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
+import { DNA } from 'react-loader-spinner'
+import Toast from './Toast'
 
 export default function Post() {
     const [postId,setPostId]=useState(1)
     const [loading,setLoading]=useState()
     const [title,setTitle]=useState('')
     const [toast,setToast]=useState({type:'info',message:''})
+
   return (
-    <div>Post</div>
+    <div>
+        <input type="number" value={postId}  />
+        {loading?<DNA/>:title}
+        <Toast type={toast.type} message={toast.message}/>
+    </div>
   )
 }
 
