00f2e97ee8b6898a56a881128824fb01d956105d
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-26 14:54:02 +0430

    start post cmp

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,63 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { DNA } from "react-loader-spinner";
-import Toast from "./Toast";
+import React from 'react'
 
 export default function Post() {
-  const [postId, setPostId] = useState(1);
-  const [loading, setLoading] = useState();
-  const [title, setTitle] = useState("");
-  const [toast, setToast] = useState({ type: "info", message: "" });
-  const userAction = (type, payload) => {
-    switch (type) {
-      case "get-post-request":
-        setPostId(payload);
-        setTitle("");
-        setLoading(true);
-        break;
-      case "get-post-success":
-        setTitle(payload);
-        setLoading(false);
-        setToast({
-          type: "success",
-          message: `post idd with ${postId} loaded`,
-        });
-        break;
-      case "get-post-error":
-        setLoading(false);
-        setToast({ type: "error", message:payload});
-        break;
-      default:
-        alert("type error");
-    }
-  };
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch(
-          `https://jsonplaceholder.typicode.com/posts/${postId}`
-        );
-        const data = await res.json();
-        if (data.title) {
-          userAction("get-post-success", data.title);
-        } else {
-          throw new Error(`post with id ${postId} not founded`);
-        }
-      } catch (error) {
-        userAction("get-post-error",error.message);
-      }
-    })();
-  }, [postId]);
-  const handleChange = (e) => {
-    userAction("get-post-request", e.target.value);
-  };
   return (
-    <div>
-      <input type="number" value={postId} onChange={handleChange} />
-      {loading ? <DNA /> : title}
-      <Toast type={toast.type} message={toast.message} />
-    </div>
-  );
+    <div>Post</div>
+  )
 }
+
 
 // import React from "react";
 // import { useState } from "react";
