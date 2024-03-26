2769a333c8507e6c65fe7d14b4c6fbaf19fb083a
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-26 15:23:57 +0430

    start part two project

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,44 +1,11 @@
-
-import React, { useEffect, useState } from 'react'
-import { DNA } from 'react-loader-spinner'
-import Toast from './Toast'
+import React from 'react'
 
 export default function Post() {
-  const [postId,setPostId]=useState(1)
-  const [title,setTitle]=useState('')
-  const [loading,setLoading]=useState()
-  const [toast,setToast]=useState({type:'info',message:''})
-  useEffect(()=>{
-    (async()=>{
-      try {
-        const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-        const data=await res.json()
-        if(data.title){
-          setTitle(data.title)
-          setLoading(false)
-          setToast({type:'success',message:`post with id ${postId} loaded`})
-        }else{
-          throw new Error(`post with id ${postId} not founded`)
-        }
-      } catch (error) {
-        setLoading(false)
-        setToast({type:'error',message:error.message})
-      }
-    })()
-  },[postId])
-  const handleChange=(e)=>{
-    setTitle('')
-    setLoading(true)
-    setPostId(e.target.value)
-  }
   return (
-    <div>
-      <input type="number" value={postId} onChange={handleChange} />
-      {loading?<DNA/>:title}
-    <Toast type={toast.type} message={toast.message}/>
-    </div>
+    <div>Post</div>
   )
 }
+
 
 
 // import React from "react";
