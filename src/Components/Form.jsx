91b60a4b64c377005cd674dad7be59cdc76fb1ee
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-27 21:54:19 +0430

    handle submit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,18 +2,18 @@ import { useFormik } from "formik";
 import React from "react";
 
 export default function Form() {
-    const formik=useFormik({
-        initialValues:{
-            username:'',
-            password:''
-        }
-    })
-    console.log(formik.values)
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit:values=>console.log(values)
+  });
   return (
-    <form>
+    <form onSubmit={formik.handleSubmit}>
       <div className="mb-3">
         <label htmlFor="exampleInputEmail1" className="form-label">
-         Username
+          Username
         </label>
         <input
           type="username"
@@ -24,9 +24,6 @@ export default function Form() {
           value={formik.values.username}
           aria-describedby="emailHelp"
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleInputPassword1" className="form-label">
@@ -41,7 +38,7 @@ export default function Form() {
           name="password"
         />
       </div>
-     
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
