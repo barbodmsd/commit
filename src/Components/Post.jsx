dd44ae1cfe61800162b4d1970fd8009fbfe3885d
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-27 19:35:50 +0430

    add bootstrap

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
 
 export default function Post() {
+  const
   return (
-    <div>Post</div>
-  )
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+         Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
