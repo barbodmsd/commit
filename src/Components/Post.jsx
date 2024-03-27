9bea5ee83fcb8a443f2b9570937a565269fed017
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-27 19:56:35 +0430

    user action

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -5,6 +5,30 @@ const initialState={
     title:'',
     toast:{type:'info',message:''}
 }
+const userAction=(state,action)=>{
+    switch(action.type){
+        case 'get-post-request':
+            return{
+                ...state,
+                postId:action.payload,
+                title:'',
+                loading:true
+            }
+        case 'get-post-success':
+            return {
+                ...state,
+                title:action.payload.title,
+                loading:false,
+                toast:{type:'success',message:action.payload.message}
+            }    
+        case 'get-post-error':
+            return{
+                ...state,
+                loading:false,
+                toast:{type:'error',message:action.payload}
+            }    
+    }
+}
 export default function Post() {
   return (
     <div>Post</div>
