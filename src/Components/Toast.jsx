c475c0fceaac226a31f32f0e83b0969f91021c23
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-27 20:00:59 +0430

    paste constructor

---
+++ src/Components/Toast.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+export default function Toast() {
+  return (
+    <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"/>
+  )
+}
