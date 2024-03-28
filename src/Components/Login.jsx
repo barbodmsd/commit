0397522d73cb107cc4eecebaadc7eca2d5b28a7c
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-28 15:11:03 +0430

    handle submit and handle chnage

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,21 +7,24 @@ export default function Login() {
             [e.target.name]:e.target.value
         })
     }
+    const handleSubmit=(e)=>{
+        console.log(fields)
+        e.preventDefault()
+    }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">
-          Email address
+          username
         </label>
         <input
-          type="email"
+          type="text"
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
+          name="username"
         />
-        <div id="emailHelp" class="form-text">
-          We'll never share your email with anyone else.
-        </div>
+        
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">
@@ -29,6 +32,7 @@ export default function Login() {
         </label>
         <input
           type="password"
+          name="password"
           class="form-control"
           id="exampleInputPassword1"
         />
