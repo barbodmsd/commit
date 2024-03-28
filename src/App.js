69ca954b40363104f4b4913282bd133fb3067aba
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-28 15:28:48 +0430

    end formik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Form from './Components/Form'
 
 export default function App() {
   return (
     <div>
-
+<Form/>
     </div>
   )
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,32 +1,44 @@
+import { useFormik } from "formik";
 import React from "react";
 
 export default function Form() {
+  const formik=useFormik({
+    initialValues:{
+      username:'',
+      password:''
+    }
+  })
+  console.log(formik.values)
   return (
     <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
           Username
         </label>
         <input
           type="text"
           name="username"
-          class="form-control"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+          className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
         />
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
           Password
         </label>
         <input
           type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
           name="password"
-          class="form-control"
+          className="form-control"
           id="exampleInputPassword1"
         />
       </div>
-      <button type="submit" class="btn btn-primary">
+      <button type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
