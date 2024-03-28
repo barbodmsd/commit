11561c655e0660b1869546dd1f7ce15e25b0af1f
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-28 15:07:10 +0430

    paste htmls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import Form from './Components/Form'
 
 export default function App() {
   return (
-    <div>
-      
-      <Form/>
-    </div>
+    <div>App</div>
   )
 }
