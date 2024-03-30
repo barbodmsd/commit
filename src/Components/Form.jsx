ce8063a920771c56824d719694f5bb98bd7c098c
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-30 12:58:55 +0430

    start learn work with formik

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,49 +1,7 @@
-import { useFormik } from "formik";
-import React from "react";
+import React from 'react'
 
-const initialValues = {
-  username: "",
-  password: "",
-};
-const onSubmit = (values) => console.log(values);
-const validate = (values) => {};
 export default function Form() {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-  });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Username
-        </label>
-        <input
-          type="text"
-          name="username"
-          onChange={formik.handleChange}
-          value={formik.values.username}
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          name="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  );
+    <div>Form</div>
+  )
 }
