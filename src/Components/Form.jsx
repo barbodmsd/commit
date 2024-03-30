fc650e91614530916a148143c957252b2cbe3d4e
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-30 13:12:12 +0430

    end managing states form and submission

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,56 +1,60 @@
 import { useFormik } from "formik";
 import React from "react";
 
-const initialValue = {
+const initialValues = {
   username: "",
   password: "",
 };
 const onSubmit = (values) => console.log(values);
 export default function Form() {
-  const formik = useFormik({ initialValue, onSubmit });
+  const formik = useFormik({ initialValues, onSubmit });
   return (
     <div>
-      For
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
             Username
           </label>
           <input
             name="username"
             onChange={formik.handleChange}
+            value={formik.values.username}
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
           />
-          <div id="emailHelp" class="form-text">
+          <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
           </div>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
             Password
           </label>
           <input
             name="password"
-            onChange={handleChange}
+            onChange={formik.handleChange}
+            value={formik.values.password}
             type="password"
-            class="form-control"
+            className="form-control"
             id="exampleInputPassword1"
           />
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1 " />
-          <label class="form-check-label" for="exampleCheck1">
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1 "
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
             Check me out
           </label>
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
-      m
     </div>
   );
 }
