2dc5f600037307142c7f9227d4065ab337837747
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-30 13:02:48 +0430

    initial value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Form from "./Components/Form";
-import Login from "./Components/Login";
 
 export default function App() {
   return (
     <div>
-      {/* <Form/> */}
-      <Login />
+      <Form/>
+      
     </div>
   );
 }
