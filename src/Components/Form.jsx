d7420f0d4805a60f501b5bbe82d8dc483b4124ed
Barbodmsd <barbodmasoudi@gmail.com>
2024-03-31 18:24:05 +0430

    paste html

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Form() {
   return (
-    <div>Form</div>
-  )
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import { useFormik } from "formik";
 // import React from "react";
@@ -80,7 +83,7 @@ export default function Form() {
 //           />
 //           {formik.errors.password&&<div className="message">{formik.errors.password}</div>}
 //         </div>
-        
+
 //         <button type="submit" className="btn btn-primary">
 //           Submit
 //         </button>
