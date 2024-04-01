979d4336fd28f98e817952e74d93556688ca6f70
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-01 16:12:37 +0430

    start formik project

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,60 +1,11 @@
-import { useFormik } from "formik";
-import React from "react";
-const initialValues = {
-  username: "",
-  password: "",
-};
-const onSubmit = (values) => {
-  console.log(values)
-  
-};
-const validate = (values) => {
-  let errors = {};
-  !values.username && (errors.username = "Username is required");
-  !values.password && (errors.password = "Password is required");
+import React from 'react'
 
-  return errors;
-};
 export default function Form() {
-  const formik = useFormik({ initialValues, onSubmit, validate });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Username
-        </label>
-        <input
-          type="text"
-          name="username"
-          onChange={formik.handleChange}
-          value={formik.values.username}
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div>{formik.errors.username&&<div className="message">{formik.errors.username}</div>}</div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
-        </label>
-        <input
-        name="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-                <div>{formik.errors.password&&<div className="message">{formik.errors.password}</div>}</div>
-
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  );
+    <div>Form</div>
+  )
 }
+
 
 // import { useFormik } from "formik";
 // import React from "react";
