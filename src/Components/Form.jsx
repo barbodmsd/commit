d65f5b4560df057052f825be301ae76b30370e70
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-08 23:26:32 +0430

    get fields props

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -12,39 +12,35 @@ const validationSchema = Yup.object({
 });
 export default function Form() {
   const formik = useFormik({ initialValues, onSubmit, validationSchema });
-  console.log(formik.touched)
+  console.log(formik.touched);
   return (
     <form>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">
-          username
+          Username
         </label>
         <input
+          name="username"
+          {...formik.getFieldProps("username")}
           type="text"
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
         />
-        <div id="emailHelp" class="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">
-          Password
+          E-mail
         </label>
         <input
-          type="password"
+        name="email"
+        {...formik.getFieldProps("email")}
+          type="email"
           class="form-control"
           id="exampleInputPassword1"
         />
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
-      </div>
+
       <button type="submit" class="btn btn-primary">
         Submit
       </button>
