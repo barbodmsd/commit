23ca49781694091f34a7971b7f2b3e4a45c0e922
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 10:15:56 +0430

    user action

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -9,31 +9,52 @@ export default function Post() {
   const [title, setTitle] = useState();
   const [toast, setToast] = useState({ type: "info", message: "" });
 
-  useEffect( () => {
-   (async()=>{
-    try {
+  const userAction = (type, payload) => {
+    switch (type) {
+      case "get-post-request":
+        setLoading(true);
+        setTitle("");
+        setPostId(payload);
+        break;
+      case "get-post-success":
+        setLoading(false);
+        setToast({ type: "success", message: payload.message });
+        setTitle(payload.title);
+        break;
+      case "get-post-error":
+        setLoading(false);
+        setToast({
+          type: "error",
+          message: `post with id ${postId} not founded`,
+        });
+        break;
+      default:
+        alert("type error");
+    }
+  };
+  useEffect(() => {
+    (async () => {
+      try {
         const res = await fetch(
           `https://jsonplaceholder.typicode.com/posts/${postId}`
         );
         const data = await res.json();
         if (data.title) {
-          setLoading(false);
-          setTitle(data.title);
-          setToast({ type: "success", message: `post with id ${postId} loaded` });
+          userAction("get-post-success", {
+            title: data.title,
+            message: `post with id ${postId} loaded`,
+          });
         } else {
           throw new Error(`post with id ${postId} not founded `);
         }
       } catch (error) {
-        setLoading(false);
-        setToast({ type: "error", message: error.message });
+        userAction("get-post-error", error.message);
       }
-   })()
+    })();
   }, [postId]);
-  const handleChange=(e)=>{
-    setPostId(e.target.value)
-    setLoading(true)
-    setTitle('')
-  }
+  const handleChange = (e) => {
+    userAction("get-post-request", e.target.value);
+  };
   return (
     <div>
       <input type="number" onChange={handleChange} />
