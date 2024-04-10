e9f51dd880a7bf9505fee4f38398c85bb82c4ee8
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 13:42:03 +0430

    update login cmp

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React from "react";
+import useFormFields from "../Utils/useFormFields";
 
 export default function Login() {
+    const [fields,handleChange]=useFormFields()
   return (
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Useranme</label>
-    <input name='username' type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input  name='password' type="password" class="form-control" id="exampleInputPassword1"/>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  )
+    <form onSubmit={handleSubmit}>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">
+          Username
+        </label>
+        <input
+          name="username"
+          onChange={handleChange}
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">
+          Password
+        </label>
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
