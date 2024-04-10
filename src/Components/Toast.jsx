a3f21dd39e1193595d19b626910dab548a5acc04
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 09:47:42 +0430

    end toast cmp

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function Toast() {
+export default function Toast({type,message}) {
+    useEffect(()=>{
+        type!=='info'&&toast[type](message)
+    },[type,message])
   return (
     <ToastContainer
       position="top-right"
