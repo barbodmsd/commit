e587ba5407f447f82dd68c22fedb64e4bd5da20a
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 10:02:19 +0430

    states

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { type } from '@testing-library/user-event/dist/type'
+import React, { useEffect, useState } from 'react'
 
 export default function Post() {
+    const [postId,setPostId]=useState(1)
+    const [loading,setLoading]=useState()
+    const [title,setTitle]=useState()
+    const [toast,setToast]=useState({type:'info',message:''})
+
+    useEffect(async()=>{
+        try {
+            const res=await fetch(``)
+            const data=await res.json()
+            if(data.title){
+                setLoading(false)
+                setTitle(data.title)
+                setToast({type:"success",message:`post with id ${postId} loaded`})
+            }else{
+                throw new Error(`post with id ${postId} not founded `)
+            }
+        } catch (error) {
+            setLoading(false)
+            setToast({type:'error',message:error.message})
+        }
+    },[postId])
   return (
     <div>Post</div>
   )
