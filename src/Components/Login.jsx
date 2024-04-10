f6e89dc5fc493f0fba056e4d5c069968e48c1458
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 13:37:27 +0430

    paste html

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Useranme</label>
+    <input name='username' type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input  name='password' type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   )
 }
