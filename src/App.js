ce8cd82b2c98d6ede969970dc3b2fe36fedea46b
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 16:23:48 +0430

    end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
-import AuthContext from "./Utils/authContext";
-import Main from "./Components/Main";
-import Login from "./Components/Login";
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import LoginRegister from "./Pages/LoginRegister";
+import ProductDetails from "./Pages/productDetails";
+import NotFoundPage from "./Pages/NotFoundPage";
 export default function App() {
-  const [token, setToken] = useState();
-  const handleToken = (tk) => {
-    setToken(tk)
-  };
   return (
     <>
-      <AuthContext.Provider value={{token,handleToken}}>
-        {token?<Main/>:<Login/>}
-      </AuthContext.Provider>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login-register" element={<LoginRegister/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/products-details/:id" element={<ProductDetails/>}/>
+          <Route path="*" element={<NotFoundPage/>} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './footer.module.css'
+export default function Footer() {
+  return (
+    <div>
+      <h2 className={style.footerContainer}> Footer</h2>
+    </div>
+  )
+}
