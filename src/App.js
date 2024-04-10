ff84648ddd2201d0a1ea168905509e7010c40c34
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 09:57:18 +0430

    start post project again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Form from "./Components/Form";
+import Post from "./Components/Post";
 
 export default function App() {
   return (
     <div>
-      <Form/>
-      
+      <Post />
     </div>
   );
 }
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,41 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { DNA } from "react-loader-spinner";
-import Toast from "./Toast";
+import React from 'react'
 
 export default function Post() {
-  const [postId, setPostId] = useState();
-  const [title, setTitle] = useState("");
-  const [loading, setLoading] = useState();
-  const [toast, setToast] = useState({ type: "info", message: "" });
-
-  useEffect(()=>{
-    (async()=>{
-        try {
-            const res=await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-            const data=await res.json()
-            if(data.title){
-                setLoading(false)
-                setTitle(data.title)
-                setToast({type:'success',message:`post with id ${postId} loaded`})
-            }else{
-                throw new Error(`post with id ${postId} not founded`)
-            }
-        } catch (error) {
-            setLoading(false)
-            setToast({type:'error',message:error.message})
-        }
-    })()
-  },[postId])
-  const handleChange = (e) => {
-    setPostId(e.target.value);
-    setLoading(true);
-    setTitle("");
-  };
   return (
-    <div>
-      <input type="number" value={postId} onChange={handleChange} />
-      {loading ? <DNA /> : title}
-      <Toast type={toast.type} message={toast.message} />
-    </div>
-  );
+    <div>Post</div>
+  )
 }
