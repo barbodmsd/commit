55f5692d64a65e84fda687670bf6dd078afcbdb3
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 09:41:07 +0430

    handle chnage

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
+import { DNA } from 'react-loader-spinner'
+import Toast from './Toast'
 
 export default function Post() {
     const [postId,setPostId]=useState()
     const [title,setTitle]=useState('')
     const [loading,setLoading]=useState()
     const [toast,setToast]=useState({type:'info',message:''})
-    
+
+    const handleChange=(e)=>{
+        setPostId(e.target.value)
+        setLoading(true)
+        setTitle('')
+    }
   return (
-    <div>Post</div>
+    <div>
+        <input type="number" value={postId} onChange={handleChange}/>
+        {loading?<DNA/>:title}
+        <Toast type={toast.type} message={toast.message}/>
+    </div>
   )
 }
