2457cb3b06dce24955ca43de9ad7ef28a4c5456f
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 14:00:48 +0430

    end

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -3,13 +3,10 @@ import AuthContext from "../Utils/authContext";
 
 export default function User() {
   const { token,handleToken } = useContext(AuthContext);
-const handleClick=()=>{
-    handleToken(null)
-}
   return (
     <div>
       <h5>User token: {token}</h5>
-      <button className="btn btn-primary " onClick={handleClick}> click to logout</button>
+      <button className="btn btn-primary " onClick={()=>handleToken(null)}> click to logout</button>
     </div>
   );
 }
