3cd6432dacb75272badd63955534c7889e19530f
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 10:33:28 +0430

    states

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+const initialState={
+    postId:1,
+    loading,
+    title,
+    toast:{type:'info',message:''}
+}
 export default function Post() {
   return (
     <div>Post</div>
