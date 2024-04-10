37a16e549f81570e2c3aa66438476c113e5ac566
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-10 13:45:14 +0430

    fetch

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import useFormFields from "../Utils/useFormFields";
+import AuthContext from "../Utils/authContext";
 
 export default function Login() {
     const [fields,handleChange]=useFormFields()
+    const {handleToken}=useContext(AuthContext)
+    const handleSubmit=async(e)=>{
+        e.preventDefault()
+        try {
+            const res=await fetch('https://fakestoreapi.com/auth/login',{
+                method:"POST",
+                headers:{'content-type': 'application/json'},
+                body:JSON.stringify(fields)
+            })
+            const data=res.json()
+            alert('login successfully')
+            handleToken(data.token)
+        } catch (error) {
+            alert('incorrect  username or password')
+        }
+    }
   return (
     <form onSubmit={handleSubmit}>
       <div class="mb-3">
