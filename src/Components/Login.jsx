27c73636e8573d9c23dbde7374da5c6f22c5412c
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-12 20:03:36 +0430

    post method

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,23 +1,25 @@
 import React, { useContext, useEffect } from "react";
 import AuthContext from "../utils/authContext";
+import useForm from "../utils/useForm";
 
 export default function Login() {
-  const {setToken}=useContext(AuthContext)
-  useEffect(()=>{
-    (async()=>{
+  const { setToken } = useContext(AuthContext);
+  const [fields, handleChange] = useForm();
+  useEffect(() => {
+    (async () => {
       try {
-        const res=await fetch('https://fakestoreapi.com/auth/login',{
-          method:'POST',
-          headers:{'content-type':'application/json'},
-          body:JSON.stringify()
-        })
-        const data=await res.json()
-        setToken(data.token)
+        const res = await fetch("https://fakestoreapi.com/auth/login", {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(fields),
+        });
+        const data = await res.json();
+        setToken(data.token);
       } catch (error) {
-        alert(error)
+        alert(error);
       }
-    })()
-  },[])
+    })();
+  }, []);
   return (
     <form>
       <div class="mb-3">
@@ -25,6 +27,8 @@ export default function Login() {
           Username
         </label>
         <input
+          name="username"
+          onChange={handleChange}
           type="text"
           class="form-control"
           id="exampleInputEmail1"
@@ -36,6 +40,8 @@ export default function Login() {
           Password
         </label>
         <input
+          name="password"
+          onChange={handleChange}
           type="password"
           class="form-control"
           id="exampleInputPassword1"
