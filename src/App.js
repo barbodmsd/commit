54ad510f872e6de44d4dc9fb1b3299984ae871da
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-12 21:51:42 +0430

    Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import LoginRegister from "./Pages/LoginRegister";
+import Products from "./Pages/Products";
+import ProductDetails from "./Pages/ProductDetails";
+import NotFound from "./Pages/NotFound";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>} exact/>
+          <Route path="/login-register" element={<LoginRegister/>}/>
+          <Route path="/products" element={<Products/>} />
+          <Route path="/product-details" element={<ProductDetails/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+
+      </main>
+      <Footer />
+    </>
+  );
 }
