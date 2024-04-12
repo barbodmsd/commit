95512f08412950bff8533e8f644d9c16c7711eed
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-12 20:09:12 +0430

    handle submit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,50 +5,53 @@ import useForm from "../utils/useForm";
 export default function Login() {
   const { setToken } = useContext(AuthContext);
   const [fields, handleChange] = useForm();
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch("https://fakestoreapi.com/auth/login", {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          body: JSON.stringify(fields),
-        });
-        const data = await res.json();
-        setToken(data.token);
-      } catch (error) {
-        alert(error);
-      }
-    })();
-  }, []);
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    useEffect(() => {
+      (async () => {
+        try {
+          const res = await fetch("https://fakestoreapi.com/auth/login", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(fields),
+          });
+          const data = await res.json();
+          setToken(data.token);
+        } catch (error) {
+          alert(error);
+        }
+      })();
+    }, []);
+  }
   return (
-    <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
           Username
         </label>
         <input
           name="username"
           onChange={handleChange}
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
         />
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
           Password
         </label>
         <input
           name="password"
           onChange={handleChange}
           type="password"
-          class="form-control"
+          className="form-control"
           id="exampleInputPassword1"
         />
       </div>
 
-      <button type="submit" class="btn btn-primary">
+      <button type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
