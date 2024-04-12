9fe21cf9de3c55f4ae73ad87e474de5eff1433d6
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-12 19:31:06 +0430

    export Auth context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import { Route, Routes, Navigate } from "react-router-dom";
-import Home from "./Pages/Home";
-import Products from "./Pages/Products";
-import LoginRegister from "./Pages/LoginRegister";
-import ProductDetails from "./Pages/productDetails";
-import NotFoundPage from "./Pages/NotFoundPage";
+import React from 'react'
+
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login-register" element={<LoginRegister/>}/>
-          <Route path="/products" element={<Products/>}/>
-          <Route path="/products-details/:id" element={<ProductDetails/>}/>
-          <Route path="*" element={<NotFoundPage/>} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
+    <div>App</div>
+  )
 }
--- src/Components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from './footer.module.css'
-export default function Footer() {
-  return (
-    <div>
-      <h2 className={style.footerContainer}> Footer</h2>
-    </div>
-  )
-}
