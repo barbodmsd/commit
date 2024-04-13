13dbc10bcfcfb4c61d3d050b7b597a28625d81ac
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 23:47:48 +0430

    end

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import './Card.css'
 export default function Card({ products }) {
   const items = products?.map((e, index) => (
-    <div className="card" >
+    <div key={index} className="card" >
       <img
         src={e.attributes["image-urls"][0]}
         className="card-img-top"
@@ -12,7 +12,7 @@ export default function Card({ products }) {
       <div className="card-body">
         <h5 className="card-title">{e.attributes.name}</h5>
         <p className="card-text">
-          Price : {e.attributes.price/100}
+          Price : ${e.attributes.price/100}
         </p>
         <Link to={`/product-details/${e.id}/${e.attributes.name.replaceAll('','-')}`} className="btn btn-primary">
           Go somewhere
