ae8a3828b9ac5dce5effd4fc63ebc8e4c748f862
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 22:47:28 +0430

    try catch

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Products() {
+  const [products,setProducts]=useState()
+  useEffect(()=>{
+    (async()=>{
+      try {
+        
+      } catch (error) {
+        alert('')
+      }
+    })()
+  },[])
   return (
     <div>Products</div>
   )
