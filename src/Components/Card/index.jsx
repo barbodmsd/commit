a1377d6c8bd5658effd6c194ca6dd76701aeff77
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 22:56:03 +0430

    end cmp card

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function Card({products}) {
-  return (
-    <div>Card</div>
-  )
+export default function Card({ products }) {
+  const items = products?.map((e, index) => (
+    <div class="card" style="width: 18rem;">
+      <img
+        src={e.attributes["image-urls"][0]}
+        class="card-img-top"
+        alt={e.attributes.name}
+      />
+      <div class="card-body">
+        <h5 class="card-title">{e.attributes.name}</h5>
+        <p class="card-text">
+          Price : {e.attributes.price/100}
+        </p>
+        <Link to={`/product-details/${id}`} class="btn btn-primary">
+          Go somewhere
+        </Link>
+      </div>
+    </div>
+  ));
+  return <>{
+    items
+  }</>;
 }
