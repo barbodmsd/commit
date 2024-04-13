961ed866846915261a2aa4d1fdeae6fdef57226a
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 23:52:58 +0430

    state ad start use effect

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function ProductDetails() {
+  const {id}=useParams()
+  const [product,setProduct]=useState()
+  
   return (
     <div>ProductDetails</div>
   )
