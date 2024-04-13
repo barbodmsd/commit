06812520862bfc28477f2ca85cd38472bc7bfef6
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 21:42:59 +0430

    state

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Products() {
+  const [products,setProducts]=useState()
   return (
     <div>Products</div>
   )
