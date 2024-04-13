51850cfd98a6cd22227d8ca8c3b88c98d7c54ff1
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 22:49:13 +0430

    end fetching

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Products() {
-  const [products,setProducts]=useState()
-  useEffect(()=>{
-    (async()=>{
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    (async () => {
       try {
-        
+        const res = await fetch(
+          "https://sephora.p.rapidapi.com/products/v2/list?number=1&size=30&country=SG&language=en-SG&sort=sales",
+          {
+            method: "GET",
+            headers: {
+              "X-RapidAPI-Key":
+                "384ba6ddb4mshd30ea53794d2c1bp16a8e9jsn1941df36d024",
+              "X-RapidAPI-Host": "sephora.p.rapidapi.com",
+            },
+          }
+        );
+        const data = await res.json();
+        setProducts(data.data);
       } catch (error) {
-        alert('')
+        alert("Fetching Error");
       }
-    })()
-  },[])
-  return (
-    <div>Products</div>
-  )
+    })();
+  }, []);
+  return <div>Products</div>;
 }
