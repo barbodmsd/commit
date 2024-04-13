f62a9ab1414f0529028bd4524fdd6df14869c655
Barbodmsd <barbodmasoudi@gmail.com>
2024-04-13 10:20:40 +0430

    html of footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import style from "./footer.module.css";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={style.footerContainer}>
+      <h2>Footer</h2>
+    </div>
+  );
 }
