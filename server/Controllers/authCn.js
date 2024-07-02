7ac383a7fc9f94223146de3e32b13be3393ed525
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-02 13:58:54 +0430

    end auth cn

--- server/Controllers/authCn.js
+++ server/Controllers/authCn.js
@@ -1,12 +1,13 @@
-import catchAsync from "../Utils/catchAsync";
+import catchAsync from "../Utils/catchAsync.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../Models/userMd";
+import User from "../Models/userMd.js";
 
 export const register = catchAsync(async (req, res) => {
   const { password, ...others } = req?.body;
+  const image=req?.file?.filename
   const hashPassword = bcrypt.hashSync(password, 10);
-  const newUser = await User.create({ ...others, password:hashPassword });
+  const newUser = await User.create({ ...others, password: hashPassword,image });
   const token = jwt.sign(
     { role: newUser.role, id: newUser._id },
     process.env.SECRET_KEY
@@ -20,6 +21,41 @@ export const register = catchAsync(async (req, res) => {
         email: newUser.email,
         image: newUser.image,
         role: newUser.role,
+      },
+      token,
+    },
+  });
+});
+
+export const login = catchAsync(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).json({
+      status: "error",
+      message: "username or password incorrect!",
+    });
+  }
+  const passwordCompare = bcrypt.compareSync(password, user.password);
+  if (username != user.username || !passwordCompare) {
+    return res.status(400).json({
+      status: "error",
+      message: "username or password incorrect!",
+    });
+  }
+  const token = jwt.sign(
+    { role: user.role, id: user._id },
+    process.env.SECRET_KEY
+  );
+  return res.status(200).json({
+    status: "success",
+    message: "Login successfully",
+    data: {
+      user: {
+        username: user.username,
+        email: user.email,
+        image: user.image,
+        role: user.role,
       },
       token,
     },
--- server/Routes/authRoute.js
+++ server/Routes/authRoute.js
@@ -1,4 +1,10 @@
 import express from 'express'
+import { login, register } from '../Controllers/authCn.js'
+import upload from '../Utils/uploadFile.js'
 const authRoute=express.Router()
+
+authRoute.route('/register').post(upload.single('file'),register)
+authRoute.route('/').post(login)
+
 
 export default authRoute
