00a6ff7d167d5ff4eed7e8abc2293bd9ab26b7e8
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-02 11:38:59 +0430

    crete server folder

--- src/Components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import style from "./footer.module.css";
-export default function Footer() {
-  return (
-    <div className={style.footerContainer}>
-      <h2>Footer</h2>
-    </div>
-  );
-}
--- src/Pages/LoginRegister/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function LoginRegister() {
-  return (
-    <div>LoginRegister</div>
-  )
-}
