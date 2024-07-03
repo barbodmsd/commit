48ddbf79faa0de06fea33a1e765b3267dd4ddc25
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-03 12:39:55 +0430

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import AuthContext from "./Utils/authContext";
+import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import BlogDetails from "./Pages/BlogDetails";
+import User from "./Pages/User";
+import Auth from "./Pages/Auth";
+import Dashboard from "./Pages/Dashboard";
+import Home from "./Pages/Home";
 
 export default function App() {
   const [data, setData] = useState({
@@ -13,7 +20,14 @@ export default function App() {
   return (
     <>
       <AuthContext.Provider value={{ data, setData }}>
-        
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/blog-details/:id' element={<BlogDetails />} />
+          <Route path='/user/:id' element={<User />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+        </Routes>
       </AuthContext.Provider>
     </>
   );
