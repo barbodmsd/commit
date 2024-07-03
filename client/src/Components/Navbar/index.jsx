f4aaf3200365bac3a4fbb7fd56b546dcf6e4d42c
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-03 12:56:26 +0430

    start home

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Stack, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <Stack
+        direction='row'
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        sx={{
+          height: "60px",
+          px: "50px",
+          bgcolor: "teal",
+        }}>
+        {/* left side */}
+        <Stack direction={"row"} alignItems={"center"} gap={"10px"}>
+          <Link to={"/"}>
+            <Button sx={{ color: "white" }}>Home</Button>
+          </Link>
+          <Link to={"/auth"}>
+            <Button sx={{ color: "white" }}>Auth</Button>
+          </Link>
+        </Stack>
+        {/* right side */}
+        <Stack direction='row' alignItems={"center"}>
+          <Link to={"/admin-dashboard"}>
+            <Button sx={{ color: "white" }}>Panel</Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </>
+  );
 }
