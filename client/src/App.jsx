01fb5f824f37bb5f9b507436a82abf0fd3034d42
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-03 12:34:35 +0430

    create context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import AuthContext from "./Utils/authContext";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const [data, setData] = useState({
+    user: {
+      username: null,
+      role: null,
+      image: null,
+    },
+    token: null,
+  });
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AuthContext.Provider value={{ data, setData }}>
+        
+      </AuthContext.Provider>
     </>
-  )
+  );
 }
-
-export default App
---
+++ client/src/Pages/BlogDetails/index.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export default function BlogDetails() {
+  return (
+    <div>BlogDetails</div>
+  )
+}
