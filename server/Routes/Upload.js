3aa1b377c6de43b3dc5d5c20bbcbb4c9eaf6d207
Barbod Masoudi <barbodmasoudi@gmail.com>
2024-10-18 20:00:33 +0330

    update

--- server/Routes/Upload.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { deleteFile, uploadFile } from "../Controllers/uploadCn.js";
-import upload from "../Utils/uploadFile.js";
-const uploadRouter = express.Router();
-uploadRouter
-  .route("/")
-  .post( upload.single("file"), uploadFile)
-  .delete(deleteFile);
-export default uploadRouter;
